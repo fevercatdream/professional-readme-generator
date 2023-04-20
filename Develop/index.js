a97fc1d0a0ab0db9bc41ce2e8828a408e36e8d3d
fevercatdream <kuma18@uw.edu>
2023-04-20 09:29:52 -0700

    added generate markdown js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,9 @@
-// Include packages needed for this application
+// Packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// Create an array of questions for user input
+// An array of questions for user input
 const questions = [
     {
         type: "input",
@@ -77,98 +78,7 @@ Then provide examples on how to run them here.
     },
 ];
 
-const licenseBadges = {
-    "Apache 2.0 License": "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-    "Boost Software License 1.0": "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
-    "Eclipse Public License 1.0": "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
-    "GNU GPL v3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
-    "ISC License": "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
-    "The MIT License": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-    "Mozilla Public License 2.0": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
-    "SIL Open Font License 1.1": "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)",
-    "The Unlicense": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
-    "The Do What the Fuck You Want to Public License": "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)",
-    "The zlib/libpng License": "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)",
-
-}
-
-// function to generate a readme file from user input
-function generateReadmeFile(answer){
-    console.log("answer from func: ", answer);
-    const markdown = 
-    `
-# ${answer.title}
-
-## Description
-
-${answer.description}
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-
-## Installation
-
-${answer.installation}
-
-## Usage
-
-${answer.usage}
-
-Include screenshots as needed.
-
-To add a screenshot, create an \`assets/images\` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    \`\`\`md
-    ![alt text](assets/images/screenshot.png)
-    \`\`\`
-
-## Credits
-
-${answer.contribution}
-
-## License
-
-${answer.license}
-
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-${licenseBadges[answer.license]}
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-## Tests
-
-${answer.tests}
-
-## Questions
-
-How to reach me with additional questions:
-
-Github Profile: [${answer.githubUsername}](https://github.com/${answer.githubUsername})
-
-Email: [${answer.email}](mailto:${answer.email})
-    `;
-
-    return markdown;
-}
-
-// Create a function to write README file
+// A function to write README file
 function writeToFile(fileName, data) {
 
     fs.writeFile(`./output/${fileName}`, data, (err) => {
@@ -179,13 +89,13 @@ function writeToFile(fileName, data) {
     })
 }
 
-// Create a function to initialize app
+// A function to initialize app
 function init() {
     inquirer
         .prompt(questions)
         .then(answer => {
             console.log(answer);
-            writeToFile("README.md", generateReadmeFile(answer))
+            writeToFile("README.md", generateMarkdown.generateReadmeFile(answer))
         });
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,93 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// An object that returns a license badge based on which license is chosen
+const licenseBadges = {
+  "Apache 2.0 License": "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  "Boost Software License 1.0": "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+  "Eclipse Public License 1.0": "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
+  "GNU GPL v3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+  "ISC License": "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+  "The MIT License": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  "Mozilla Public License 2.0": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+  "SIL Open Font License 1.1": "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)",
+  "The Unlicense": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+  "The Do What the Fuck You Want to Public License": "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)",
+  "The zlib/libpng License": "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)",
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
 }
 
-module.exports = generateMarkdown;
+// A function to generate a readme file from user input
+function generateReadmeFile(answer){
+  console.log("answer from func: ", answer);
+  const markdown = 
+  `
+# ${answer.title}
+
+## Description
+
+${answer.description}
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${answer.installation}
+
+## Usage
+
+${answer.usage}
+
+Include screenshots as needed.
+
+To add a screenshot, create an \`assets/images\` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+  \`\`\`md
+  ![alt text](assets/images/screenshot.png)
+  \`\`\`
+
+## Credits
+
+${answer.contribution}
+
+## License
+
+${answer.license}
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+${licenseBadges[answer.license]}
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+${answer.tests}
+
+## Questions
+
+How to reach me with additional questions:
+
+Github Profile: [${answer.githubUsername}](https://github.com/${answer.githubUsername})
+
+Email: [${answer.email}](mailto:${answer.email})
+  `;
+
+  return markdown;
+}
+
+module.exports.generateReadmeFile = generateReadmeFile;
